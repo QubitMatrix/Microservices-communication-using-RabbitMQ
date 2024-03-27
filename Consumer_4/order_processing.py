8e9a3ca6fd5a7467a4fc0fa7425c27d5ad5090de
Praadnya <99022986+Praadnya@users.noreply.github.com>
2024-03-28 00:40:45 +0530

    Update order_processing.py

--- Consumer_4/order_processing.py
+++ Consumer_4/order_processing.py
@@ -7,6 +7,7 @@ channel.queue_declare(queue='order_processing')
 
 def callback(ch, method, properties, body):
     print("Order processing message received:", body.decode())
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
 channel.basic_consume(queue='order_processing', on_message_callback=callback, auto_ack=True)
 
