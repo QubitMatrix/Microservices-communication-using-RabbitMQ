c73ed0701e09ee06735a6c9f2e426527e9976cad
PrannayH <104824339+PrannayH@users.noreply.github.com>
2024-03-28 05:52:24 +0530

    Update health_check.py

--- Consumer_1/health_check.py
+++ Consumer_1/health_check.py
@@ -7,6 +7,7 @@ channel.queue_declare(queue='health_check')
 
 def callback(ch, method, properties, body):
     print("Health check message received:", body.decode())
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
 channel.basic_consume(queue='health_check', on_message_callback=callback, auto_ack=True)
 
