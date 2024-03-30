ebc7c9ab8e02f9d05d4fa767822293c155266a8a
QubitMatrix <preethi13m@gmail.com>
2024-03-31 02:16:35 +0530

    Mongo integration added for consumer_2

--- Consumer_2/item_creation.py
+++ Consumer_2/item_creation.py
@@ -1,27 +1,50 @@
 import pika
 import json
 import logging
+import pymongo
 
+# Display debug info
 logging.basicConfig(level=logging.DEBUG)
 
+# Setup rabbitmq 
 credentials = pika.PlainCredentials(username="guest", password="guest")
 parameters = pika.ConnectionParameters(host="rabbitmq", port=5672, credentials=credentials)
 connection = pika.BlockingConnection(parameters=parameters)
 channel = connection.channel()
 
+# declare insert_item queue
 channel.queue_declare(queue='insert_item', durable=True)
 
+# callback when read from queue
 def callback(ch, method, properties, body):
     body = body.decode()
     body = json.loads(body)
 
-    print(body)
+    name = body['name']
+    desc = body['description']
+    sku = body['sku']
+    cost_price = body['cost_price']
+    selling_price = body['selling_price']
+    initial_stock = body['initial_stock']
+    reorder = body['reorder_point']
+
     logging.debug("in consumer2")
-    logging.debug("{Item Name: "+body['name']+", Description: "+body['description']+", SKU: "+body['sku']+", Cost_price: "+body['cost_price']+", Selling_price: "+body['selling_price']+", Initial Stock: "+body['initial_stock']+", Reorder: "+body['reorder_point']+"}")
+    logging.debug("{Item Name: "+name+", Description: "+desc+", SKU: "+sku+", Cost_price: "+cost_price+", Selling_price: "+selling_price+", Initial Stock: "+initial_stock+", Reorder: "+reorder+"}")
+    
+    client = pymongo.MongoClient("mongodb://mongo_db:27017")
+    db = client['microservices_db']
+    collection = db['item_details']
+    record = {"Name": name, "Description": desc, "SKU": sku, "Cost Price": cost_price, "Selling Price": selling_price, "Initial Stock": initial_stock, "Reorder Point": reorder}
+    logging.debug("Before mongo insert")
+    collection.insert_one(record)
+    logging.debug("After mongo insert")
+    logging.debug("abc"+"\n".join([ str(x) for x in collection.find()]))
+    client.close()
 
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
+# config consume from insert_item queue to call callback
 channel.basic_consume(queue="insert_item", on_message_callback=callback)
 
 print("Waiting for message")
-channel.start_consuming()
+channel.start_consuming() # start consuming from the queue - infinite blocking connection
